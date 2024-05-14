7fc11d9cf1498e2c112f33e8f3c02804c7a180ef
Daniel Margarido <daniel.margarido@lockwood-publishing.com>
2024-05-14 13:57:26 +0100

    Updated the setup script to install the latest nats client version. Generated new protobuf files since the ones we had were outdated

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
-import re
+"""
+This module contains the setup script for the 'asyncio-nats-streaming' package.
+
+The setup script defines the package metadata, such as name, version, description, classifiers, etc.
+It also specifies the package dependencies and other configuration options.
+"""
 import ast
+import re
 
+from setuptools import setup
 
+# Regular expression pattern to extract the version number from the source code
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('stan/aio/client.py', 'rb') as f:
@@ -30,5 +37,5 @@ setup(
     license='Apache 2 License',
     packages=['stan', 'stan.aio', 'stan.pb'],
     zip_safe=True,
-    install_requires=['protobuf>=3.7', 'nats-py==2.7.0'],
+    install_requires=['protobuf>=3.7', 'nats-py>=2.7.0'],
 )
